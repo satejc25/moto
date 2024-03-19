5938f2738307813a305c5e5a47167b16e0d5c9d7
satej <satejcr7@gmail.com>
2024-03-19 22:37:12 +0530

    --- Auto Git Commit ---

--- modules/main.tf
+++ modules/main.tf
@@ -16,18 +16,18 @@ provider "aws" {
 }
 
 terraform {
-  backend "s3" {
-    bucket = "motogp-terraform-buckkk"
-    key = "terraform.tfstate"
-    region = "ap-south-1"
-    dynamodb_table = "motogp-terraform-tf"
+  backend "s3" {                            #backend define where to store tfstate file
+    bucket = "motogp-terraform-buckkk"      #in this bucket tfstate file is stored
+    key = "terraform.tfstate"               #file to be stored in bucket
+    region = "ap-south-1"                   #region to connect with dynamodb table
+    dynamodb_table = "motogp-terraform-tf"  #dynamodb table--> terraform generate keynames that includes values of bucket and key variable.... 
     
   }
 }
 
-module "motogp-vpc-module" {
-    source = "./vpc"
-    cidr_block = var.cidr_block
+module "motogp-vpc-module" {                #we can put our code in module block and use our code multiple times by changing values from variable.tf file. module contains all the variables declared in code 
+    source = "./vpc"                        #source is location of child directory where our resources are stored
+    cidr_block = var.cidr_block             #var.cidr block means here the valu is dynamic and it is passed through variable.tf file.  
     project = var.project
     Environment = var.Environment
     public_subnet_1a = var.public_subnet_1a
@@ -44,7 +44,7 @@ module "ec2" {
     ami_id = var.ami_id
     key_name_global = var.key_name_global
     instance_type_mgp = var.instance_type_mgp
-    subnet_id_mgp = module.motogp-vpc-module.public_subnet_id
+    subnet_id_mgp = module.motogp-vpc-module.public_subnet_id         #here we have called values from module. syntax for calling module is {{module.module_name.output_name}}.. Here output name is important because module calling is based on output block because in output block we have printed our id's arns's and names. 
     security_groups = [module.motogp-vpc-module.security_groups]
     project = var.project
     Environment = var.Environment
