47a315bd58ae0b1dcf804779d831e242df84c655
satej <satejcr7@gmail.com>
2024-03-19 15:03:19 +0530

    --- Auto Git Commit ---

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -4,7 +4,6 @@ resource "aws_instance" "moto-ec2" {
     instance_type = var.instance_type_mgp
     subnet_id = var.subnet_id_mgp
     ebs_optimized = true
-    vpc_security_group_ids = var.vpc_security_group_ids_motogp
     tags = {
       Name = "${var.project}"
       Environment = var.Environment
