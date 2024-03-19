1e18d4e7023e15ac399174c0dc0c4d4df0a8c0d9
satej <satejcr7@gmail.com>
2024-03-19 20:22:47 +0530

    --- Auto Git Commit ---

--- modules/variable.tf
+++ modules/variable.tf
@@ -63,7 +63,7 @@ variable "avalability_zone_1a" {
   
 }
 
-variable "avalability_zone_2b" {
+variable "avail_zone_2a" {
     description = "avalability of vpc"
     type = string
     default = "us-east-2b"
