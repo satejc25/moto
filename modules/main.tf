d6fdc299eba2902440bd8c6b7ff240d5563c6906
satej <satejcr7@gmail.com>
2024-03-19 20:26:47 +0530

    --- Auto Git Commit ---

--- modules/main.tf
+++ modules/main.tf
@@ -33,7 +33,7 @@ module "motogp-vpc-module" {
     public_subnet_1a = var.public_subnet_1a
     public_subnet_1b = var.public_subnet_1b
     avalability_zone_1a = var.avalability_zone_1a
-    avalability_zone_2b = var.avalability_zone_2b
+    avail_zone_2a = var.avail_zone_2a
     private_subnet_1a = var.private_subnet_1a
     cidr_ipv4_block = var.cidr_block
 
