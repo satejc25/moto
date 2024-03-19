2dfe5e61863bb8724c7f744230530401b1168ee4
satej <satejcr7@gmail.com>
2024-03-19 15:44:39 +0530

    --- Auto Git Commit ---

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -67,7 +67,37 @@ resource "aws_route_table_association" "private_subnet_association" {
   
 }
 
-output "public_subnet_id" {
-    value = aws_subnet.public_subnet.id
+#creating security group
+
+resource "aws_security_group" "motogp-sg" {
+    name = "${var.project}-vpc"
+    description = "allow inbound and outbound traffic"
+    vpc_id = aws_vpc.moto_vpc.arn
+    tags = {
+     Name = "allow_tls"
+  }
   
 }
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.motogp-sg.id
+  cidr_ipv4         = var.cidr_ipv4_block
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
+  security_group_id = aws_security_group.motogp-sg.id
+  cidr_ipv4         = var.cidr_ipv4_block
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.motogp-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
